8e84d4ca0fbee50acba81fb132238f3c7fe4c586
Suyash Mahar <suyash12mahar@outlook.com>
2022-04-10 09:16:07 -0700

    Fix tests and circleci

--- src/scripts/tests/run.sh
+++ src/scripts/tests/run.sh
@@ -3,16 +3,22 @@ export PMEM_END_ADDR=0x20000000000
 export CXLBUF_RUNNING_TESTS=1
 
 DIR="$(dirname $(readlink -f "$0"))"
+ROOT_DIR="$(readlink -f "$DIR/../../../")"
 PMEM_LOC="${PMEM_LOC:-/mnt/pmem0/}"
 LOG_LOC="${PMEM_LOC}/cxlbuf_logs"
 
 beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 
+echo "# Found following tests file: "
 suite() {
     for f in "$DIR"/test_*; do
-        echo "$f"
+        echo "#   $f"
         . "$f"
     done
 }
+
+echo '--------------'
+echo 'Starting tests'
+echo '--------------'
 
 . "$DIR/../../../vendor/shunit2"
--- src/scripts/tests/test_recovery__linkedlist.sh
+++ src/scripts/tests/test_recovery__linkedlist.sh
@@ -8,16 +8,18 @@ if [ -z "$CXLBUF_RUNNING_TESTS" ]; then
 fi
 
 test_recovery_linkedlist() {
-    rm -rf "$LOG_LOC" "${PMEM_LOC}/"linkedlist*
+    BIN="${ROOT_DIR}/src/examples/linkedlist_tx/linkedlist_tx"
+    POOL="${PMEM_LOC}/linkedlist"
+    rm -rf "$LOG_LOC" "$POOL"*
 
     printf "> Creating a pool\n"
-    echo "i 1" | /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null 1>&2
+    echo "i 1" | "$BIN" "$POOL" 2>/dev/null 1>&2
 
     printf "> Crashing on insertion\n"
-    echo "p\ni 2" | CXLBUF_CRASH_ON_COMMIT=1  /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null 1>&2
+    echo "p\ni 2" | CXLBUF_CRASH_ON_COMMIT=1  "$BIN" "$POOL" 2>/dev/null 1>&2
 
     printf "> Recovering\n"
-    recOut=$(echo "p" | /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null)
+    recOut=$(echo "p" | "$BIN" "$POOL" 2>/dev/null)
 
     recMatch=`echo "$recOut" | grep -Eo '\\\$ 1 \$'`
     assertEquals '$ 1 ' "${recMatch}"
@@ -29,14 +31,15 @@ test_recovery_linkedlist_multiple() {
     rm -rf "$LOG_LOC" "${PMEM_LOC}/"linkedlist*
 
     printf "> Creating a pool\n"
-    echo "i 1\ni 2\ni 3" | /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null 1>&2
+    echo "i 1\ni 2\ni 3" | "$BIN" "$POOL" 2>/dev/null 1>&2
 
     printf "> Crashing on insertion\n"
-    echo "i 10" | CXLBUF_CRASH_ON_COMMIT=1  /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null 1>&2
+    echo "i 10" | CXLBUF_CRASH_ON_COMMIT=1  "$BIN" "$POOL" 2>/dev/null 1>&2
 
     printf "> Recovering\n"
-    recOut=$(echo "p" | /home/smahar/git/cxlbuf/src/examples/linkedlist_tx/linkedlist_tx /mnt/pmem0/linkedlist 2>/dev/null)
+    recOut=$(echo "p" | "$BIN" "$POOL" 2>/dev/null)
     recMatch=`echo "$recOut" | grep -Eo '\\\$ 2 3 1 \$'`
+
     assertEquals '$ 2 3 1 ' "${recMatch}"
 }
 suite_addTest test_recovery_linkedlist_multiple
