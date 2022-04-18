3219707b327f3ec1a562ad6d4d41d79df8a63ebc
Suyash Mahar <suyash12mahar@outlook.com>
2022-04-18 08:37:01 -0700

    Add volatile and non-volatile results to simplekv

--- src/scripts/simplekvtest.sh
+++ src/scripts/simplekvtest.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SIMPLEKV_ROOT="/home/smahar/git/cxlbuf/src/examples/"
+CONFIG_FILE="${SIMPLEKV_ROOT}/../../make.config"
 SIMPLEKV_CXLBUF="simplekv_cxlbuf/simplekv_puddles"
 SIMPLEKV_PMDK="simplekv_pmdk/simplekv_pmdk"
 YCSB_LOC="/home/smahar/git/libpuddles-scripts/traces/"
@@ -15,7 +16,35 @@ export PMEM_END_ADDR=0x20000000000
 export CXL_MODE_ENABLED=0
 
 set -e
-# set -x
+set -o pipefail
+#set -x
+
+recompile() {
+    make clean -C "$(dirname "$CONFIG_FILE")"
+    make release -j$(nproc) -C "$(dirname "$CONFIG_FILE")"
+}
+
+remake() {
+    if [ -z "$LLVM_DIR" ]; then
+        echo "LLVM_DIR not set"
+        exit 1
+    fi
+
+   
+    if [ "$1" = "nonvolatile" ]; then
+        sed -i 's|LOG_FORMAT_VOLATILE=y|LOG_FORMAT_VOLATILE=n|g' "$CONFIG_FILE"
+        sed -i 's|LOG_FORMAT_NON_VOLATILE=n|LOG_FORMAT_NON_VOLATILE=y|g' "$CONFIG_FILE"
+    else
+        sed -i 's|LOG_FORMAT_VOLATILE=n|LOG_FORMAT_VOLATILE=y|g' "$CONFIG_FILE"
+        sed -i 's|LOG_FORMAT_NON_VOLATILE=y|LOG_FORMAT_NON_VOLATILE=n|g' "$CONFIG_FILE"
+    fi
+
+    if [ "$V" = "1" ]; then 
+        recompile
+    else
+        recompile >/dev/null 2>&1
+    fi
+}
 
 execute() {
     rm -f "${PMDK_POOL}"*
@@ -32,7 +61,7 @@ execute() {
 
     rm -f "${PMDK_POOL}"*
     rm -f "${LOG_LOC}"
-    # fallocate --length 1G /mnt/pmem0/simplekv
+
     CXL_MODE_ENABLED=1 "${SIMPLEKV_ROOT}/${SIMPLEKV_CXLBUF}.inst" "${PMDK_POOL}" \
                        ycsb "${YCSB_LOC}${wrkld}-load-1.0" \
                        "${YCSB_LOC}${wrkld}-run-1.0"  2>&1 \
@@ -42,15 +71,18 @@ execute() {
 
     rm -f "${PMDK_POOL}"*
     rm -f "${LOG_LOC}"
-    # fallocate --length 1G /mnt/pmem0/simplekv
-    # "${SIMPLEKV_ROOT}/${SIMPLEKV_CXLBUF}" "${PMDK_POOL}" ycsb \
-                                        # "${YCSB_LOC}${wrkld}-load-1.0" \
-                                        # "${YCSB_LOC}${wrkld}-run-1.0"  2>&1 \
-        # | grep 'Total ns' | grep -Eo '[0-9]+' | tr -d '\n'
 
     printf "\n"
 }
 
+remake volatile
+printf "pmdk,snashot,msync\n"
+for wrkld in $YCSB_WRKLD; do
+    execute
+done
+
+
+remake nonvolatile
 printf "pmdk,snashot,msync\n"
 for wrkld in $YCSB_WRKLD; do
     execute
