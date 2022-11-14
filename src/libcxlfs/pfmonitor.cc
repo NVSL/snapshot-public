5ff9204c10aadd650b3b02a0f6fd4600b3b216d4
Suyash Mahar <suyash12mahar@outlook.com>
2022-11-14 09:43:02 -0800

    Add pf monitor tests
    * pfmonitor.h: Move PFMonitor class declaration to pfmonitor.h
    * test_pfmonitor.cc: Add new tests

--- src/libcxlfs/pfmonitor.cc
+++ src/libcxlfs/pfmonitor.cc
@@ -15,6 +15,7 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 
+#include "libcxlfs/pfmonitor.hh"
 #include "nvsl/common.hh"
 #include "nvsl/error.hh"
 
@@ -22,122 +23,111 @@ int userfaultfd(int flags) {
   return syscall(SYS_userfaultfd, flags);
 }
 
-class PFMonitor {
-public:
-  using addr_t = uint64_t;
-  using Callback = std::function<void(addr_t)>;
+int PFMonitor::error(std::string err_str, bool has_errno /* = true */) {
+  if (has_errno) {
+    DBGE << PSTR() << ": " << err_str << std::endl;
+  } else {
+    DBGE << err_str << std::endl;
+  }
 
-private:
-  int pf_fd = -1;
+  return -1;
+}
 
-  int error(std::string err_str, bool has_errno = true) {
-    if (has_errno) {
-      DBGE << PSTR() << ": " << err_str << std::endl;
-    } else {
-      DBGE << err_str << std::endl;
+/** @brief Monitor fd for page faults. Blocking. **/
+int PFMonitor::monitor_fd_blocking(int fd, Callback &cb) {
+  struct pollfd evt = {};
+  struct uffd_msg fault_msg = {0};
+
+  evt.fd = fd;
+  evt.events = POLLIN;
+
+  while (1) {
+    int pollval = poll(&evt, 1, 10);
+
+    switch (pollval) {
+    case -1:
+      perror("poll/userfaultfd");
+      continue;
+    case 0:
+      continue;
+    case 1:
+      DBGH(2) << "Handling page fault!" << std::endl;
+      break;
+    default:
+      DBGE << "Unexpected poll result" << std::endl;
+      return -1;
     }
 
+    /* unexpected poll events */
+    if (evt.revents & POLLERR) {
+      return error("++ POLLERR");
+    } else if (evt.revents & POLLHUP) {
+      return error("++ POLLHUP");
+    }
+
+    if (read(fd, &fault_msg, sizeof(fault_msg)) != sizeof(fault_msg)) {
+      return error("ioctl_userfaultfd read");
+    }
+
+    char *place = (char *)fault_msg.arg.pagefault.address;
+    DBGH(3) << "Got page fault at " << (void *)(place) << std::endl;
+
+    /* handle the page fault */
+    if (fault_msg.event & UFFD_EVENT_PAGEFAULT) {
+      struct uffdio_range range = {};
+      auto page_aligned = ((uint64_t)place / 4096) * 4096;
+      range.start = page_aligned;
+      range.len = 4096;
+
+      cb(range.start);
+
+      if (ioctl(fd, UFFDIO_WAKE, &range) == -1) {
+        perror("ioctl/wake");
+        exit(1);
+      } else {
+        std::cerr << "fault handled\n";
+      }
+    }
+  }
+}
+
+  /** @brief Get the pagefault file descriptor **/
+int PFMonitor::get_pf_fd() {
+  int pf_fd = -1;
+
+  auto error = [](std::string err_str, bool has_errno = true) {
+    DBGE << PSTR() << ": " << err_str << std::endl;
     return -1;
   };
 
-  /** @brief Monitor fd for page faults. Blocking. **/
-  int monitor_fd_blocking(int fd, Callback &cb) {
-    struct pollfd evt = {};
-    struct uffd_msg fault_msg = {0};
-
-    evt.fd = fd;
-    evt.events = POLLIN;
-
-    while (1) {
-      int pollval = poll(&evt, 1, 10);
-
-      switch (pollval) {
-      case -1:
-        perror("poll/userfaultfd");
-        continue;
-      case 0:
-        continue;
-      case 1:
-        DBGH(2) << "Handling page fault!" << std::endl;
-        break;
-      default:
-        DBGE << "Unexpected poll result" << std::endl;
-        return -1;
-      }
-
-      /* unexpected poll events */
-      if (evt.revents & POLLERR) {
-        return error("++ POLLERR");
-      } else if (evt.revents & POLLHUP) {
-        return error("++ POLLHUP");
-      }
-
-      if (read(fd, &fault_msg, sizeof(fault_msg)) != sizeof(fault_msg)) {
-        return error("ioctl_userfaultfd read");
-      }
-
-      char *place = (char *)fault_msg.arg.pagefault.address;
-      DBGH(3) << "Got page fault at " << (void *)(place) << std::endl;
-
-      /* handle the page fault */
-      if (fault_msg.event & UFFD_EVENT_PAGEFAULT) {
-        struct uffdio_range range = {};
-        auto page_aligned = ((uint64_t)place / 4096) * 4096;
-        range.start = page_aligned;
-        range.len = 4096;
-
-        cb(range.start);
-
-        if (ioctl(fd, UFFDIO_WAKE, &range) == -1) {
-          perror("ioctl/wake");
-          exit(1);
-        } else {
-          std::cerr << "fault handled\n";
-        }
-      }
-    }
+  /* Open a userfaultd file descriptor */
+  if ((pf_fd = userfaultfd(O_NONBLOCK)) == -1) {
+    return error(": ++ userfaultfd failed");
   }
 
-  /** @brief Get the pagefault file descriptor **/
-  int get_pf_fd() {
-    int pf_fd = -1;
-
-    auto error = [](std::string err_str, bool has_errno = true) {
-      DBGE << PSTR() << ": " << err_str << std::endl;
-      return -1;
-    };
-
-    /* Open a userfaultd file descriptor */
-    if ((pf_fd = userfaultfd(O_NONBLOCK)) == -1) {
-      return error(": ++ userfaultfd failed");
-    }
-
-    /* Check if the kernel supports the read/POLLIN protocol */
-    struct uffdio_api uapi = {};
-    uapi.api = UFFD_API;
-    if (ioctl(pf_fd, UFFDIO_API, &uapi)) {
-      return error(": ++ ioctl(fd, UFFDIO_API, ...) failed");
-    }
-
-    if (uapi.api != UFFD_API) {
-      return error(": ++ unexepcted UFFD api version.");
-    }
-
-    return pf_fd;
+  /* Check if the kernel supports the read/POLLIN protocol */
+  struct uffdio_api uapi = {};
+  uapi.api = UFFD_API;
+  if (ioctl(pf_fd, UFFDIO_API, &uapi)) {
+    return error(": ++ ioctl(fd, UFFDIO_API, ...) failed");
   }
 
-public:
-  PFMonitor() {}
-
-  /** @brief Initialize the internal state **/
-  int init() {
-    this->pf_fd = get_pf_fd();
-
-    return this->pf_fd;
+  if (uapi.api != UFFD_API) {
+    return error(": ++ unexepcted UFFD api version.");
   }
+
+  return pf_fd;
+}
+
+/** @brief Initialize the internal state **/
+int PFMonitor::init() {
+  this->pf_fd = get_pf_fd();
+
+  return this->pf_fd;
+}
 
   /** @brief Monitor for page faults and handle them using cb **/
-  int monitor(Callback cb) {
+  int PFMonitor::monitor(Callback cb) {
     int res = 0;
 
     NVSL_ASSERT(pf_fd != -1, "init() not called for PFMonitor");
@@ -152,22 +142,33 @@ public:
     return res;
   }
 
-  /** @brief Register an address range with PFMonitor **/
-  int register_range(void *start, size_t range) {
+  /**
+   * @brief Register an address range with PFMonitor
+   * @param[in] start Starting address of the range to monitor
+   * @param[in] len Length of the range in bytes
+   *
+   * @return 0 if range was registered successfuly. Underlying ioctl() return
+   * code otherwise.
+   *
+   * @details Address start and start+size should be page aligned.
+   **/
+  int PFMonitor::register_range(void *start, size_t len) {
+    struct uffdio_register reg = {};
+    int res;
+
     NVSL_ASSERT(pf_fd != -1, "init() not called for PFMonitor");
 
-    struct uffdio_register reg = {};
+    DBGH(3) << "Register range start " << start << " len " << len << "\n";
 
     reg.mode = UFFDIO_REGISTER_MODE_MISSING;
     reg.range = {};
     reg.range.start = (unsigned long long)start;
-    reg.range.len = (unsigned long long)range;
+    reg.range.len = (unsigned long long)len;
 
-    int res = ioctl(this->pf_fd, UFFDIO_REGISTER, &reg);
+    res = ioctl(this->pf_fd, UFFDIO_REGISTER, &reg);
     if (res != 0) {
       return error("ioctl(fd, UFFDIO_REGISTER, ...) failed");
     }
 
     return 0;
   }
-};
