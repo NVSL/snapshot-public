96cfbe6814a8eb908a68992c9d28012852250aea
Suyash Mahar <suyash12mahar@outlook.com>
2022-11-27 10:58:27 -0800

    Fix deps in makefile, add linux cfg in readme and missing deps in deps.sh

--- src/scripts/deps.sh
+++ src/scripts/deps.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-APT_PKG_LIST="libvulkan-dev libisal-dev build-essential git python3 gcc-10 g++-10 emacs zsh"
+APT_PKG_LIST="libnuma-dev libcunit1 libcunit1-doc libcunit1-dev libaio-dev libncurses-dev libssl-dev uuid-dev python3-pip pkg-config meson libvulkan-dev libisal-dev build-essential git python3 gcc-10 g++-10 emacs zsh"
 
 SUDO="${SUDO:-sudo}"
 if [ "$EUID" -eq 0 ]; then
    export SUDO=""
 fi
 
+pip install pyelftools
+
+"${SUDO}" apt update
 "${SUDO}" apt install -y $APT_PKG_LIST
 "${SUDO}" chsh -s /bin/zsh "${USER}"
