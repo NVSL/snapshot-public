0042d3c3107da14d0df7910188993721bff02729
Suyash Mahar <suyash12mahar@outlook.com>
2022-11-28 21:23:29 -0800

    Add linux 6.0.10 config and update deps.sh

--- src/scripts/deps.sh
+++ src/scripts/deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-APT_PKG_LIST="gdb libnuma-dev libcunit1 libcunit1-doc libcunit1-dev libaio-dev libncurses-dev libssl-dev uuid-dev python3-pip pkg-config meson libvulkan-dev libisal-dev build-essential git python3 gcc-10 g++-10 emacs zsh"
+APT_PKG_LIST="flex bison libelf-dev dwarves zstd cmake gdb libnuma-dev libcunit1 libcunit1-doc libcunit1-dev libaio-dev libncurses-dev libssl-dev uuid-dev python3-pip pkg-config meson libvulkan-dev libisal-dev build-essential git python3 gcc-10 g++-10 emacs zsh"
 
 SUDO="${SUDO:-sudo}"
 if [ "$EUID" -eq 0 ]; then
@@ -11,4 +11,4 @@ pip install pyelftools
 
 "${SUDO}" apt update
 "${SUDO}" apt install -y $APT_PKG_LIST
-"${SUDO}" chsh -s /bin/zsh "${USER}"
+#"${SUDO}" chsh -s /bin/zsh "${USER}"
